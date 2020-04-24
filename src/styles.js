94ada71451f19b2d3b981b8ef255c08cd4e3a107
tunahanoguz <tunahanog@gmail.com>
2020-04-24 10:08:40 +0300

    Styles created.
    Colors and gradients created inside styles.js file!

---
+++ src/styles.js
@@ -0,0 +1,23 @@
+export const colors = {
+  blue: '#0094FC',
+  green: '#5ED40A',
+  disconnectedGrey: '#CECECE',
+  textGrey: '#8B959A',
+  light: '#FDFDFD',
+  dark: '#011F3F',
+  darkOpacity: 'rgba(1, 31, 63, 0.1)',
+  selectedPageGrey: '#919BA0',
+  unselectedPageGrey: 'rgba(145, 155, 160, 0.3)',
+  shadow: 'rgba(0, 0, 0, 0.1)',
+  middleCircleBlue: 'rgba(0, 0, 0, 0.3)',
+  innerCircleGrey: 'rgba(255, 255, 255, 0.2)',
+  selectedCircle: '#DEDEDE',
+  unselectedCircle: '#249DF6',
+};
+
+export const gradients = {
+  lightBlueGradient: ['#2861FF', '#25B5FF'],
+  darkBlueGradient: ['#0055FF', '#0094FB'],
+  lightGreyGradient: ['#C2CDED', '#A2A3A4'],
+  darkGreyGradient: ['#6E7383', '#505557'],
+};
