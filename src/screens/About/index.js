a3e0ff791aca851f1efc49a915177990c9a02a28
tunahanoguz <tunahanog@gmail.com>
2020-04-24 10:12:56 +0300

    Created initial empty screen to configure react navigation.

---
+++ src/screens/About/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+function About() {
+  return (
+    <View>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+export default About;
