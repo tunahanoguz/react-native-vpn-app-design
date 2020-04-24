e6444c634a5944d5aba487e68891cf6b86e32954
tunahanoguz <tunahanog@gmail.com>
2020-04-24 16:00:27 +0300

    Drawer styles updated.

--- src/navigations/drawerNavigation/index.js
+++ src/navigations/drawerNavigation/index.js
@@ -2,12 +2,23 @@ import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeNavigation from '../homeNavigation';
 import { About } from '../../screens';
+import theme from '../../theme';
 
 function DrawerNavigation() {
   const Drawer = createDrawerNavigator();
 
   return (
-    <Drawer.Navigator initialRouteName="Introduction">
+    <Drawer.Navigator
+      initialRouteName="Introduction"
+      drawerContentOptions={{
+        activeBackgroundColor: 'transparent',
+        labelStyle: {
+          fontFamily: 'SF-Pro-Display-Medium',
+        },
+        inactiveTintColor: theme.colors.textGrey,
+        activeTintColor: theme.colors.dark,
+        overlayColor: 0.1,
+      }}>
       <Drawer.Screen name="Home" component={HomeNavigation} />
       <Drawer.Screen name="About" component={About} />
     </Drawer.Navigator>
