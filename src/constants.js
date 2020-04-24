612769cdd4cae6f69b47ea8ca3ad04293bab1896
tunahanoguz <tunahanog@gmail.com>
2020-04-24 09:43:11 +0300

    Constants created.
    Countries and texts constants created inside constants.js!

---
+++ src/constants.js
@@ -0,0 +1,55 @@
+export const countries = [
+  {
+    id: 1,
+    icon: require('./assets/icons/random-country.png'),
+    name: 'Automatic',
+  },
+  {
+    id: 2,
+    icon: require('./assets/icons/united-states.png'),
+    name: 'New York, NY',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/united-kingdom.png'),
+    name: 'London',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/russia.png'),
+    name: 'Moscow',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/sweden.png'),
+    name: 'Sweden',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/australia.png'),
+    name: 'Melbourne',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/india.png'),
+    name: 'New Delhi',
+  },
+  {
+    id: 1,
+    icon: require('./assets/icons/singapore.png'),
+    name: 'Singapore',
+  },
+];
+
+export const texts = {
+  appTitle: 'VPN',
+  introTitle: 'Secured, forever.',
+  introDescription:
+    'Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis.',
+  introButton: 'GET STARTED',
+  connected: 'Connected',
+  disconnected: 'Disconnected',
+  connect: 'Connect now',
+  disconnect: 'Disconnect',
+  pickServer: 'Pick your Server',
+};
