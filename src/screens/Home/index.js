6f1d64016a7fa61ea6a57ac5b9d8e7a766584821
tunahanoguz <tunahanog@gmail.com>
2020-04-24 15:05:34 +0300

    Home and Introduction screens updated.
    Home and Introduction screens updated to complete sketch design.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,63 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import { Image } from 'react-native';
+import { Box, Button, VPNButton, Text } from '../../components';
+import { countries, texts } from '../../constants';
+import theme from '../../theme';
 
 function Home() {
+  const [isConnected, setIsConnected] = React.useState(false);
+
+  function toggleVPN() {
+    setIsConnected((value) => !value);
+  }
+
   return (
-    <View>
-      <Text>Home!</Text>
-    </View>
+    <Box flex={1} bg="white">
+      <Box flex={0.9} justifyContent="center">
+        <Button
+          flexDirection="row"
+          alignItems="center"
+          mb={8}
+          py={2}
+          px={5}
+          isShadow={true}
+          bg="white">
+          <Text medium color="textGrey">
+            {texts.connected}
+          </Text>
+          <Box width={8} height={8} ml={0} bg="green" borderRadius={100} />
+        </Button>
+
+        <VPNButton isConnected={isConnected} setIsConnected={setIsConnected} />
+
+        <Button
+          borderWidth={isConnected ? '1px' : '0px'}
+          borderColor="borderDark"
+          bg={isConnected ? 'transparent' : 'blue'}
+          onPress={toggleVPN}>
+          <Text variant="bold" uppercase color={!isConnected && 'white'}>
+            {texts.disconnect}
+          </Text>
+        </Button>
+      </Box>
+
+      <Box
+        flex={0.1}
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        bg="light"
+        css={{
+          shadowColor: theme.colors.shadow,
+          shadowOffset: { width: 0, height: 5 },
+          elevation: 100,
+        }}>
+        <Image source={require('../../assets/icons/united-states.png')} />
+        <Text fontSize={2} ml={5}>
+          {countries[1].name}
+        </Text>
+      </Box>
+    </Box>
   );
 }
 
--- src/screens/Introduction/index.js
+++ src/screens/Introduction/index.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import { Image, StatusBar } from 'react-native';
+import { Box, Button, Text } from '../../components';
+import { texts } from '../../constants';
 
-function Introduction() {
+function Introduction({ navigation }) {
   return (
-    <View>
-      <Text>Home!</Text>
-    </View>
+    <Box flex={1} justifyContent="center" alignItems="center" bg="white" px={9}>
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+      <Image source={require('../../assets/images/secured-man.png')} />
+
+      <Box alignItems="center" mt={12} mb={10}>
+        <Text variant="semiBold" fontSize={4} mb={1}>
+          {texts.introTitle}
+        </Text>
+
+        <Text fontSize={[1]} textAlign="center" color="textGrey">
+          {texts.introDescription}
+        </Text>
+      </Box>
+
+      <Button
+        flexDirection="row"
+        bg="blue"
+        onPress={() => navigation.navigate('Home')}>
+        <Text uppercase variant="bold" fontSize={[0]} color="white">
+          {texts.introButton}
+        </Text>
+      </Button>
+    </Box>
   );
 }
 
